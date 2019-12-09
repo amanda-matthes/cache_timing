dcbca45ce87b3c12ddb75ca04213aee066058348
Amanda Matthes <amanda.matthes@gmail.com>
2019-12-09 00:12:03 +0000

    cleaned up attempt_access.cpp

--- attempt_access.cpp
+++ attempt_access.cpp
@@ -1,3 +1,17 @@
+// compile with g++ -std=c++11 -IC:\\boost\\include\boost-1_71\\ -o attempt_access.exe attempt_access.cpp
+
+/*************************************
+ * This process uses a shared_memory_object 
+ * (see boost library "interprocess") called
+ * "shared_memory" to access an int at a given
+ * memory location and print it.
+ * 
+ * The process will crash if "shared_memory"
+ * does not exist or if there is a segmentation
+ * fault.
+ * 
+ *************************************/
+
 #include <stdio.h>
 #include <windows.h>
 #include <stdlib.h>
@@ -16,39 +30,32 @@ int main(int argc, char *argv[]){
     // Map the shared_memory in this process
     boost::interprocess::mapped_region region(shared_memory, boost::interprocess::read_write);
 
-    printf("Started attempt_access.exe\n");
 
-    printf("Let's first check out the shared memory\n");
-    printf("--------------------------------\n");
-    printf("SHARED MEMORY\n");
-    printf("Region start: %08x \n", region.get_address());
-    printf("Region size:  %i   \n", region.get_size());
+    // printf("Let's first check out the shared memory\n");
+    // printf("--------------------------------\n");
+    // printf("SHARED MEMORY\n");
+    // printf("Region start: %08x \n", region.get_address());
+    // printf("Region size:  %i   \n", region.get_size());
+
+    // printf("Memory  : Value\n");
+    // int *mem = static_cast<int*>(region.get_address());
+    // for(std::size_t i = 0; i < region.get_size()/sizeof(int); ++i){
+    //     printf("%08x: %08x\n", mem, *mem);
+    //     mem++;
+    // }
+    // printf("--------------------------------\n \n");
+    // printf("Now let's try to access the requested location\n");
+
+    int * ptr = (int *) atoi(argv[1]);  
+
+    // printf("Starting at %08x aka %s\n", ptr, argv[1]);
+    // printf("Memory  : Value\n");
+    // for (int i = 0; i < 10; i++) {
+    //     printf("%08x : %08x\n", ptr, *ptr);
+    //     ptr++;
+    // }  
 
     printf("Memory  : Value\n");
-    int *mem = static_cast<int*>(region.get_address());
-    for(std::size_t i = 0; i < region.get_size()/sizeof(int); ++i){
-        printf("%08x: %08x\n", mem, *mem);
-        mem++;
-    }
-    printf("--------------------------------\n \n");
+    printf("%08x : %08x\n", ptr, *ptr);
 
-    printf("Now let's try to access the requested location\n");
-
-    int * ptr = (int *) atoi(argv[1]);  // 0x0060fe10; //
-
-    printf("Starting at %08x aka %s\n", ptr, argv[1]);
-    
-    // Sleep(5000);      // wait 1min 
-
-    printf("Memory  : Value\n");
-    for (int i = 0; i < 10; i++) {
-        printf("%08x : %08x\n", ptr, *ptr);
-        ptr++;
-    }  
-
-    printf("done. no segmentation faults.\n");
-
-    printf("FINISHED ATTEMPT_ACCESS \n");
-    printf("----------------------------\n");
-    printf("----------------------------\n");
 }
